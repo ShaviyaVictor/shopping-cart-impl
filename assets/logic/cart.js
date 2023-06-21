47976bdd4c5b05895dc731cb5f566cdab857afdf
ShaviyaVictor <shaviyavictor@gmail.com>
2023-06-20 21:29:30 -0500

    Refactor the code for ease of maintenability

---
+++ assets/logic/cart.js
@@ -0,0 +1,14 @@
+
+let basket = JSON.parse(localStorage.getItem("basketItems")) || [];
+// console.log(basket)
+
+
+// items in cart count 
+let totalItems = () => {
+  let cartItems = document.getElementById("cart-items");
+  // * reduce function takes in the previous Num and the next Num and sums them up and adds them on the the defined default value of 0
+  cartItems.innerHTML = basket.map((item) => item.items)
+                              .reduce((prevNum, nextNum) => prevNum + nextNum, 0)
+  
+};
+totalItems(); 
