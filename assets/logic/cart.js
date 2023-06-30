ccbb4f2d25fce9c6e295ed37b661db096256b96b
ShaviyaVictor <shaviyavictor@gmail.com>
2023-06-30 18:51:43 -0500

    Create the logic for the else statement for the 2nd zero items in the cart

--- assets/logic/cart.js
+++ assets/logic/cart.js
@@ -2,6 +2,7 @@
 // * call the html sections for population
 let cartItem = document.getElementById("cart-item");
 let summary = document.getElementById("summary");
+let backShopping = document.getElementById("shop");
 
 let basket = JSON.parse(localStorage.getItem("basketItems")) || [];
 // console.log(basket)
@@ -25,9 +26,16 @@ let showCartItems = () => {
   // capture the scenarios using the if-else statement 
   if (basket.length !== 0) {
     console.log("The basket has some items")
-  }
-  else {
-    console.log("The basket is empty...")
+  } else {
+    // console.log("The basket is empty...")
+    summary.innerHTML = ``;
+    cartItem.innerHTML = `
+      <a href="/index.html" class="flex font-semibold text-indigo-600 text-sm mt-10">        
+        <svg class="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"/></svg>
+         Go Shopping
+      </a>
+    `;
+    backShopping.innerHTML = ``;
   }
 }
 showCartItems();
