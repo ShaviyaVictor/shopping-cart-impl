ed68db448c2892e07649b4155392d654961d9c6d
ShaviyaVictor <shaviyavictor@gmail.com>
2023-07-02 02:53:55 -0500

    Retrieve the items code from the js file and restructure the code

--- assets/logic/cart.js
+++ assets/logic/cart.js
@@ -33,30 +33,7 @@ let showCartItems = () => {
       let search = catalogueItems.find((item) => item.id === id) || [];
       return `
 
-      <div class="flex w-2/5">
-      <div class="w-20">
-        <img class="h-24" src="https://drive.google.com/uc?id=18KkAVkGFvaGNqPy2DIvTqmUH_nk39o3z" alt="">
-      </div>
-      <div class="flex flex-col justify-between ml-4 flex-grow">
-        <span class="font-bold text-sm">Iphone 6S</span>
-        <span class="text-red-500 text-xs">Apple</span>
-        <a href="#" class="font-semibold hover:text-red-500 text-gray-500 text-xs">Remove</a>
-      </div>
-    </div>
-    <!-- quantity change section -->
-    <div class="flex justify-center w-1/5">
-      <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
-      </svg>
-
-      <input class="mx-2 border text-center w-8" type="text" value="1">
-
-      <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
-        <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
-      </svg>
-    </div>
-    <!-- unit price and total  -->
-    <span class="text-center w-1/5 font-semibold text-sm">$400.00</span>
-    <span class="text-center w-1/5 font-semibold text-sm">$400.00</span>
+      
 
       `
     }).join("")
